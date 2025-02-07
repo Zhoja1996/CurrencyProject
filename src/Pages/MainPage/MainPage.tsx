477f24242802b7caec07f720792d73891c104adb
Zhoja1996 <g.n.kowal@gmail.com>
2025-02-07 19:42:22 +0200

    add CurrencyPage(edit media to Navbar)

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -5,7 +5,7 @@ import Navbar from "../../components/Navbar/Navbar";
 
 const MainPage = () => {
     return (
-        <div className="bg-[url(https://obmen24.com.ua/wp-content/uploads/2024/09/front-page_kamyanske2.jpg)] bg-center bg-[length:150%] bg-no-repeat media-bg">
+        <div className="bg-[url(https://obmen24.com.ua/wp-content/uploads/2024/09/front-page_kamyanske2.jpg)] bg-center bg-no-repeat bg-cover">
             <Navbar/>
             <Main/>
             <Footer/>
